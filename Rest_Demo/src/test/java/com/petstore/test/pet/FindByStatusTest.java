04d6f28d485e6461f289d18139292501df29fbaf
Mariia99 <57643321+Mariia99@users.noreply.github.com>
2020-04-16 20:01:21 +0300

    Update FindByStatusTest.java

--- Rest_Demo/src/test/java/com/petstore/test/pet/FindByStatusTest.java
+++ Rest_Demo/src/test/java/com/petstore/test/pet/FindByStatusTest.java
@@ -14,7 +14,7 @@ import static io.restassured.RestAssured.given;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 public class FindByStatusTest {
-    public static final String URI = "http://192.168.99.100:8080";
+    public static final String URI = "http://35.224.235.72:8080";
 
     @Test
     void testContentType() {
